4a8c6ea8cd7f096006f407b6a898c9433a2bf9a2
xnero13-id <66856177+Readone99@users.noreply.github.com>
2021-10-15 14:10:46 +0700

    Add files via upload

---
+++ Bam.php
@@ -0,0 +1,57 @@
+
+
+<htm><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<meta name="viewport" content="width=device-width, initial-scale=1" /> 
+<head>	
+<title>Hacked By NERO13</title>
+<meta property="og:description" content="=[ "CIREBON BLACKHAT ]="/>
+		<meta property="og:image" content="https://g.top4top.io/p_1928ku5qq1.png"/>
+		<link rel="shortcut icon" href="https://g.top4top.io/p_1928ku5qq1.png"/>
+		<link href="https://fonts.googleapis.com/css?family=&display=swap" type="text/css" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Audiowide&display=swap" type="text/css" rel="stylesheet">
+		</head>
+		<body oncontextmenu="return false" onkeydown="return false" onmousedown="return false">
+		<script type="text/javascript">var snowmax=35var snowcolor=new Array("#AAAACC","#DDDDFF","#CCCCDD","#F3F3F3","#F0FFFF")var snowtype=new Array("Arial Black","Arial Narrow","Times","Comic Sans MS")var snowletter="*"var sinkspeed=0.6var snowmaxsize=22var snowminsize=8var snowingzone=1
+		// Do not edit below this linevar snow=new Array()var marginbottomvar marginrightvar timervar i_snow=0var x_mv=new Array();var crds=new Array();var lftrght=new Array();var browserinfos=navigator.userAgent var ie5=document.all&&document.getElementById&&!browserinfos.match(/Opera/)var ns6=document.getElementById&&!document.allvar opera=browserinfos.match(/Opera/) var browserok=ie5||ns6||operafunction randommaker(range) 
+		{			rand=Math.floor(range*Math.random()) return rand}function initsnow() {	if (ie5 || opera) {		marginbottom = document.body.clientHeight		marginright = document.body.clientWidth	
+		}	else if (ns6) {		marginbottom = window.innerHeight		marginright = window.innerWidth	}	var snowsizerange=snowmaxsize-snowminsize	for (i=0;i<=snowmax;i ) 
+		{		crds[i] = 0; 	lftrght[i] = Math.random()*15; 	x_mv[i] = 0.03 Math.random()/10;		snow[i]=document.getElementById("s" i)		snow[i].style.fontFamily=snowtype[randommaker(snowtype.length)]		snow[i].size=randommaker(snowsizerange) snowminsize		snow[i].style.fontSize=snow[i].size		snow[i].style.color=snowcolor[randommaker(snowcolor.length)]		snow[i].sink=sinkspeed*snow[i].size/5		
+		if (snowingzone==1) {snow[i].posx=randommaker(marginright-snow[i].size)}		if (snowingzone==2) {snow[i].posx=randommaker(marginright/2-snow[i].size)}		if (snowingzone==3) {snow[i].posx=randommaker(marginright/2-snow[i].size) marginright/4}		
+		if (snowingzone==4) {snow[i].posx=randommaker(marginright/2-snow[i].size) marginright/2}		snow[i].posy=randommaker(2*marginbottom-marginbottom-2*snow[i].size)		snow[i].style.left=snow[i].posx		snow[i].style.top=snow[i].posy	}	
+		movesnow()}function movesnow() {	for (i=0;i<=snowmax;i ) {		crds[i] = x_mv[i];		snow[i].posy =snow[i].sink		snow[i].style.left=snow[i].posx lftrght[i]*Math.sin(crds[i]);		snow[i].style.top=snow[i].posy		if (snow[i].posy>=marginbottom-2*snow[i].size || parseInt(snow[i].style.left)>(marginright-3*lftrght[i]))
+		{			if (snowingzone==1) {snow[i].posx=randommaker(marginright-snow[i].size)}			
+		if (snowingzone==2) {snow[i].posx=randommaker(marginright/2-snow[i].size)}			
+		if (snowingzone==3) {snow[i].posx=randommaker(marginright/2-snow[i].size) marginright/4}			
+		if (snowingzone==4) {snow[i].posx=randommaker(marginright/2-snow[i].size) marginright/2}			
+		snow[i].posy=0		}	}	var timer=setTimeout("movesnow()",50)}for (i=0;i<=snowmax;i ) {	document.write("<span id='s" i "' style='position:absolute;top:-" snowmaxsize "'>" snowletter "</span>")}if (browserok) 
+		{	window.onload=initsnow}</script>
+		<head> 
+		<title>Hacked By GanestSeven</title>
+		<style type="text/css">.lagu{background:transparent;border:1px solid red;font-family:Share\ Tech\ Mono;color:;font-size:10px;font-weight:normal;padding:2px 25px;text-decoration:none;text-shadow:0 0 0px #15cff4}</style>
+		<script> function play(){var audio=document.getElementById('lagu');audio.play();}function liat(){document.getElementById('galiat').style.visibility='visible';}</script>
+		<script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript">
+		</script><style>img[alt="www.000webhost.com"]{display:none;}</style>
+		</style>
+		<style type='text/css'>HTML,BODY{cursor: none;}</style>
+		<body BGCOLOR="black">
+		<center>
+		<body background="" height="" width=""><center>
+		<style> h1 { text-align: center; color: #3335cf; font-size: 35px; font-family: Arial Narrow, sans-serif; font-style: garamond; text-shadow: 100vmax #f00505;> text-shadow: 0px 0px 0px #f00505;> </style> 
+		<center>
+		<br>
+		<img src="https://i.ibb.co/c3ftMpj/20210804-194820.png" height="300" width="300">
+		<br>
+		<br>
+		</center>
+		<center>
+		<b><font color=red size="3" face=courier New>Hacked By <br><font color=white>Reggy Gans Feat Xnero13</b><hr>
+		<blink><font color=white>==[ <b>Ganest Seven</b> ]==</font></blink>
+		<hr>
+		<center>
+		<font color=red size=1 face="audiowide">AKU PAYAH KALI INI WAJAHKU MERAH MATAKU MERAH BIBIRKU MERAH SURGAWI SEMOGA DIA JODOHNYA</font>
+		<hr><b><font size=1 color="white"> Thanks To & Brotherhoods </marquee></b><hr> <font face="audiowide" size="2px">  || Ganest Sevent || Cirebon Blackhat || Cirebon Xploit || Saitama Crew Xploit || <br> || Purwokerto Xploit || Rft || Lamer Blackhat || King Of King || <br> || DemonNipon || Bogor Getar || Jakarta Cyber Security || DragonBolld || Loser Black Diamond || <br>
+		<div class='widget-content'><iframe width="0" height="0" src="https://f.top4top.io/m_18171le1s2.mp3" frameborder="0" allowfullscreen auotoplay></iframe></div></div></div>
+		</center></audio> </font><br><br> 
+		</font></b></i></td> </tr> </tbody> </table> </center> </body></html>
+		
